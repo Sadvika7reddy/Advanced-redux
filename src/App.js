3ce5651314b51365b3d5c72989fb4bb257240db0
sadvika7reddy <sadvika7reddy>
2023-02-22 11:54:10 +0530

    network calls

--- src/App.js
+++ src/App.js
@@ -1,14 +1,57 @@
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { cartAction } from './store/Cart';
+import { Fragment } from 'react';
+import Notification from './components/UI/Notification';
+let initial=true;
 function App() {
  const isVisible= useSelector(state=>state.ui.cartisVisible)
+ const cart=useSelector(state=>state.display)
+ const notification=useSelector(state=>state.ui.notification)
+ const dispatch=useDispatch();
+
+ useEffect(()=>{
+  const eventHandler= async()=>{
+    let response=await fetch("https://add-movies-c908f-default-rtdb.firebaseio.com/cart.json",{
+      method:'PUT',
+      body:JSON.stringify(cart)
+     })
+   if(!response.ok)
+   {
+    dispatch(cartAction.responses({
+      status:"error",
+      message:"error occures",
+      title:"sending failed"
+    }))
+   }
+   if(initial){
+    initial=false
+    return;
+   }
+   dispatch(cartAction.responses({
+    status:"success",
+    message:"succes",
+    title:"request send"
+   }))
+
+  }
+  eventHandler();
+}
+ ,[cart,dispatch])
+
   return (
+    <Fragment>
+      {notification&&<Notification status={notification.status}
+      title={notification.title}
+      message={notification.message}/>}
     <Layout>
-      {!isVisible&&<Cart />}
+      {isVisible&&<Cart />}
       <Products />
     </Layout>
+    </Fragment>
   );
 }
 
--- src/store/Cart.js
+++ src/store/Cart.js
@@ -1,10 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 const cartSlice=createSlice({
     name:"cart",
-    initialState:{cartisVisible:false},
+    initialState:{cartisVisible:false,notification:null},
     reducers:{
         toggle(state){
             state.cartisVisible=!state.cartisVisible
+        },
+        responses(state,action){
+          state.notification={
+            status:action.payload.status,
+            message:action.payload.message,
+            title:action.payload.title
+
+          }  
         }
     }
 })
